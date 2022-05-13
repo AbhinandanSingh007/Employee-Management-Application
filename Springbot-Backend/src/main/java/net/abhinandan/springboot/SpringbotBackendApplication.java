5a48bb5b5d4e268bb1b183502b94b5724758af9b
Abhinandan Singh <abhinandan.k.singhh@gmail.com>
2022-05-13 15:56:46 -0700

    Initial commit

---
+++ Springbot-Backend/src/main/java/net/abhinandan/springboot/SpringbotBackendApplication.java
@@ -0,0 +1,13 @@
+package net.abhinandan.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotBackendApplication.class, args);
+	}
+
+}
